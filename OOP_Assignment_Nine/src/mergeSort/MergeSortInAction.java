8b817d8f229a270466afc8de0d6d2800236f4f32
jackd <jackd@Jacks_PC.attlocal.net>
2024-10-24 16:43:21 -0400

    Implementation of merge sort

--- OOP_Assignment_Nine/src/mergeSort/MergeSortInAction.java
+++ OOP_Assignment_Nine/src/mergeSort/MergeSortInAction.java
@@ -1,10 +1,48 @@
 package mergeSort;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import sortClass.Sort;
+
 public class MergeSortInAction {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		int[] array;
+		
+		if(args.length == 1) {
+			array = Sort.readFileToArray(args[0]);
+			System.out.println("The array that has been inputed is: ");
+			for(int i = 0; i < array.length; i++) {
+				System.out.println(array[i]);
+			}
+		}else {
+			Scanner in = new Scanner(System.in);
+			System.out.println("You have not provided an array, so a random one will be created. How many integers would you like there to be?");
+			int arraySize = 0;
+			try {
+				arraySize = in.nextInt();
+			}catch(InputMismatchException e) {
+				System.out.println("You have entered an invalid input.");
+			}
+			in.close();
+			array = Sort.createRandomArray(arraySize);
+			System.out.println("The array that has been created is: ");
+			for(int i = 0; i < array.length; i++) {
+				System.out.println(array[i]);
+			}
+		}
+		
+		Sort.mergeSort(array, 0, array.length - 1);
+		System.out.println("This is the array in order from smallest to largest: ");
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+		Sort.writeArrayToFile(array, "output.txt");
+		System.out.println("The sorted array has been recorded in the file: 'output.txt'.");
+		
 	}
 
 }
--- OOP_Assignment_Nine/src/sortClass/Sort.java
+++ OOP_Assignment_Nine/src/sortClass/Sort.java
@@ -1,5 +1,120 @@
 package sortClass;
 
-public class Sort {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Sort {
+	
+	public static int numLines;
+	
+	public static int[] createRandomArray(int arrayLength) {
+		
+		int[] array = new int[arrayLength];
+		for(int i = 0; i < arrayLength; i++) {
+			array[i] = (int) (Math.random() * 101);
+		}
+		
+		return array;
+		
+	}
+	
+	public static void writeArrayToFile(int[] array, String filename) throws IOException {
+		
+		int[] arrayOut = array;
+			
+		FileWriter output = new FileWriter(filename);
+		
+		for(int i : arrayOut) {
+			output.write(i + "\n");
+		}
+		
+		output.close();
+		
+	}
+	
+	public static int[] readFileToArray(String filename) throws IOException {
+		
+		File file = new File(filename);
+		Scanner input = new Scanner(file);
+		
+		numLines = 0;
+		
+		while(input.hasNextInt()) {
+			input.nextLine();
+			numLines++;
+		}
+		
+		input.close();
+		int[] array = new int[numLines];
+		Scanner in = new Scanner(file);
+		
+		for(int i = 0; i < numLines; i++) {
+			array[i] = in.nextInt();
+		}
+		
+		in.close();
+		
+		return array;
+		
+	}
+	
+	public static void mergeSort(int[] array, int left, int right) {
+		
+		if(left < right) {
+			int middle = (left + (right - 1)) / 2;
+			
+			mergeSort(array, left, middle);
+			mergeSort(array, middle + 1, right);
+			
+			merge(array, left, middle, right);
+		}
+		
+	}
+	
+	public static void merge(int[] array, int left, int middle, int right) {
+		
+		int leftArraySize = middle - left + 1;
+		int rightArraySize = right - middle;
+		
+		int[] leftArray = new int[leftArraySize];
+		int[] rightArray = new int[rightArraySize];
+		
+		for(int i = 0; i < leftArraySize; i++) {
+			leftArray[i] = array[left + i];
+		}
+		for(int j = 0; j < rightArraySize; j++) {
+			rightArray[j] = array[middle + 1 + j];
+		}
+		
+		int i = 0, j = 0;
+		
+		int k = left;
+		
+		while(i < leftArraySize && j < rightArraySize) {
+			if(leftArray[i] <= rightArray[j]) {
+				array[k] = leftArray[i];
+				i++;
+			}else {
+				array[k] = rightArray[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i < leftArraySize) {
+			array[k] = leftArray[i];
+			i++;
+			k++;
+		}
+		
+		while(j < rightArraySize) {
+			array[k] = rightArray[j];
+			j++;
+			k++;
+		}
+		
+	}
+	
 }
